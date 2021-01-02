482c2baaadaad7d0caceb480d78e404f95980cab
skwebs <skwebserv@gmail.com>
2021-01-02 07:01:09 +0530

    File upload.

---
+++ ci4offc/app/Controllers/Site.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class Site extends BaseController
+{
+	public function index()
+	{
+		return view('v1/index');
+	}
+
+	//--------------------------------------------------------------------
+
+}
